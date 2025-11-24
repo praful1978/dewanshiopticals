82deea79000155a1fabdb4a5597f5741b636b5a1
praful1978 <93826951+praful1978@users.noreply.github.com>
2025-11-24 10:37:47 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
+const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected!'))
-.catch(err => console.error('MongoDB connection error:', err));
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("Mongo Error: " + err));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server Working!");
+});
+
+// Listen
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Backend running on " + PORT));
